a8caa9007148e0b2e6d27a9a8b42a0171cdbbfec
Yassine <54895428+yassine955@users.noreply.github.com>
2019-12-22 14:53:52 +0100

    Public Log

---
+++ public/precache-manifest.93331c2a240d9f856a326b4dc146e75f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7c79768a79fc9883b3197bef113c8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a22855442e5bc844f731",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "84106a3f0a804d4f3cc0",
+    "url": "/static/js/2.68409063.chunk.js"
+  },
+  {
+    "revision": "786089ef6a78275eeee7a67aba60641a",
+    "url": "/static/js/2.68409063.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a22855442e5bc844f731",
+    "url": "/static/js/main.a7d9a606.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
